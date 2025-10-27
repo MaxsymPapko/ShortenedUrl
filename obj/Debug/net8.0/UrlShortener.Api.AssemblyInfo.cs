1bd6893a396c55fa243ae5ec083e02a11e391cd0
Maxsym Papko <papkom50@gmail.com>
2025-10-27 17:53:24 +0200

    Create an admin role

--- obj/Debug/net8.0/UrlShortener.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/UrlShortener.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+715c160c687e5c22be14656f6e2dbee975522a17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c4c4e8efa72ec1697c354185c0cdb4c78a59ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
