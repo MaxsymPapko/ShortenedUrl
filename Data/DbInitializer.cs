6b53023a31bcc8b4bcc1c3ff6b56132c5088786b
Maxsym Papko <papkom50@gmail.com>
2025-10-27 17:46:57 +0200

    Created admin user at DbInitializer cs

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UrlShortener.Api.Models;
+
+namespace UrlShortener.Api.Data
+{
+    public static class DbInitializer
+    {
+        public static async Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string adminRole = "admin";
+            if (!await roleManager.RoleExistsAsync(adminRole))
+            {
+                await roleManager.CreateAsync(new IdentityRole(adminRole));
+            }
+
+            var adminEmail = "admin@local.test";
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+            if (adminUser == null)
+            {
+                adminUser = new ApplicationUser { UserName = adminEmail, Email = adminEmail };
+                await userManager.CreateAsync(adminUser, "Admin123!"); // заміни пароль якщо потрібно
+                await userManager.AddToRoleAsync(adminUser, adminRole);
+            }
+        }
+    }
+}
