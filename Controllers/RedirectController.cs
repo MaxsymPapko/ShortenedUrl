6728b528ad0536fa817e1272de229849130e6ae8
Maxsym Papko <papkom50@gmail.com>
2025-10-28 00:28:44 +0200

    add a redirect controller logic for redirecting from shortened url to original one

---
+++ Controllers/RedirectController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Api.Data;
+
+namespace UrlShortener.Api.Controllers
+{
+    [Route("")]
+    public class RedirectController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public RedirectController(ApplicationDbContext db) => _db = db;
+
+        [HttpGet("{shortCode}")]
+        public async Task<IActionResult> RedirectToOriginal(string shortCode)
+        {
+            var item = await _db.ShortUrls.FirstOrDefaultAsync(x => x.ShortCode == shortCode);
+            if (item == null) return NotFound("Short code not found.");
+            return Redirect(item.OriginalUrl);
+        }
+    }
+}
