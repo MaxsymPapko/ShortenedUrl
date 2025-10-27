95b1b03fbe54aa6c9ed72645f9358463380d333f
Maxsym Papko <papkom50@gmail.com>
2025-10-27 17:08:16 +0200

    Initial commit

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UrlShortener.Api.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ Models/ShortUrl.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UrlShortener.Api.Models
+{
+    public class ShortUrl
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string OriginalUrl { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        public string ShortedUrl { get; set; } = null!;
+
+        [Required]
+        public string CreatedById { get; set; } = null!;
+        public ApplicationUser? CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
